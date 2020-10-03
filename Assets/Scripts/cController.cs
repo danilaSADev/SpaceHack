b8e28e199aee856b40ce0432912ac323d2c6ea41
danilaSADev <61512362+danilaSADev@users.noreply.github.com>
2020-10-03 18:17:36 +0300

    Initial commit

---
+++ Assets/Scripts/cController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cController : MonoBehaviour
+{
+    float vertical;
+    float horizontal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello world!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        vertical = Input.GetAxisRaw("Vertical");
+        horizontal = Input.GetAxisRaw("Horizontal");
+        transform.position += new Vector3(horizontal, 0, vertical ) * Time.deltaTime;
+        Debug.Log("Y: " + vertical + " ; X: " + horizontal);
+    }
+}
